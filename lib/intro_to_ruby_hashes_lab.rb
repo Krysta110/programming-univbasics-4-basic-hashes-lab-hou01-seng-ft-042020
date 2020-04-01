7ec629b375afd238f5d4039401876a15680371c2
Krysta Wolkowiecki <krystawolkowiecki@outlook.com>
2020-04-01 06:58:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,8 +7,8 @@ end
 def my_hash
   # return a valid hash with any key/value pair of your choice
   krysta = {
-    :age => 35,
-    :hair => brown,
+    :age => "35",
+    :hair => "brown",
   }
 end
 
